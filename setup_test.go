4fc3a4acf07e24cb23da8f0e785eb3dd2d82af96
lgz <ilyamaschenko@gmail.com>
2019-02-04 09:40:36 +0300

    fix tests

--- setup_test.go
+++ setup_test.go
@@ -59,7 +59,7 @@ func TestPlugin_SetupBrokenConfig(t *testing.T) {
 	p.Option = &cli.Option{}
 	p.ConfigPath = multipath.New("./testdata")
 	p.Registry = module.Registry{"module1": module.Creator{}}
-	p.configName = "go.d.conf-broken.yml"
+	p.configName = "test.d.conf-broken.yml"
 	assert.False(t, p.Setup())
 }
 
@@ -69,7 +69,7 @@ func TestOrchestrator_SetupEmptyConfig(t *testing.T) {
 	o.Option = &cli.Option{Module: "all"}
 	o.ConfigPath = multipath.New("./testdata")
 	o.Registry = module.Registry{"module1": module.Creator{}}
-	o.configName = "go.d.conf-empty.yml"
+	o.configName = "test.d.conf-empty.yml"
 
 	assert.True(t, o.Setup())
 }
@@ -80,7 +80,7 @@ func TestOrchestrator_SetupDisabledInConfig(t *testing.T) {
 	o.Option = &cli.Option{}
 	o.ConfigPath = multipath.New("./testdata")
 	o.Registry = module.Registry{"module1": module.Creator{}}
-	o.configName = "go.d.conf-disabled.yml"
+	o.configName = "test.d.conf-disabled.yml"
 	assert.False(t, o.Setup())
 }
 
@@ -90,7 +90,7 @@ func TestOrchestrator_SetupNoModulesToRun(t *testing.T) {
 	o.Option = &cli.Option{Module: "module3"}
 	o.ConfigPath = multipath.New("./testdata")
 	o.Registry = module.Registry{"module1": module.Creator{}}
-	o.configName = "go.d.conf.yml"
+	o.configName = "test.d.conf.yml"
 
 	assert.Len(t, o.modules, 0)
 	assert.False(t, o.Setup())
@@ -103,7 +103,7 @@ func TestOrchestrator_SetupSetGOMAXPROCS(t *testing.T) {
 	o.ConfigPath = multipath.New("./testdata")
 	o.Registry = module.Registry{"module1": module.Creator{}, "module2": module.Creator{}}
 	o.Config.MaxProcs = 1
-	o.configName = "go.d.conf.yml"
+	o.configName = "test.d.conf.yml"
 	assert.True(t, o.Setup())
 	assert.Equal(t, o.Config.MaxProcs, runtime.GOMAXPROCS(0))
 }
@@ -118,7 +118,7 @@ func TestOrchestrator_Setup(t *testing.T) {
 		"module1": module.Creator{},
 		"module2": module.Creator{},
 		"module3": module.Creator{}}
-	o.configName = "go.d.conf.yml"
+	o.configName = "test.d.conf.yml"
 	assert.True(t, o.Setup())
 	assert.Len(t, o.modules, 3)
 
@@ -132,7 +132,7 @@ func TestOrchestrator_Setup(t *testing.T) {
 		"module2": module.Creator{},
 		"module3": module.Creator{DisabledByDefault: true},
 	}
-	o.configName = "go.d.conf.yml"
+	o.configName = "test.d.conf.yml"
 	assert.True(t, o.Setup())
 	assert.Len(t, o.modules, 2)
 
@@ -142,7 +142,7 @@ func TestOrchestrator_Setup(t *testing.T) {
 	o.Option = &cli.Option{Module: "module2"}
 	o.ConfigPath = multipath.New("./testdata")
 	o.Registry = module.Registry{"module1": module.Creator{}, "module2": module.Creator{}}
-	o.configName = "go.d.conf.yml"
+	o.configName = "test.d.conf.yml"
 	assert.True(t, o.Setup())
 	assert.Len(t, o.modules, 1)
 
