89520df0cdb738beb675c9c96cfbbc748e8807da
ilyam8 <ilya@netdata.cloud>
2019-08-23 09:44:07 +0300

    job statuses saver

---
+++ save_test.go
@@ -0,0 +1,26 @@
+package orchestrator
+
+import (
+	"bytes"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_jobStatusesSaveLoop(t *testing.T) {
+	b := &bytes.Buffer{}
+	js := newJobsStatuses()
+	s := newJobsStatusesSaver(b, js, 1)
+
+	js.put(&mockJob{}, "active")
+	s.runOnce()
+	go func() {
+		time.Sleep(time.Second * 3)
+		s.stop()
+	}()
+	go s.mainLoop()
+	<-s.stopCh
+
+	assert.NotZero(t, b.Bytes())
+}
