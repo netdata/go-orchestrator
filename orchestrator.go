6fdf3c168843478e0cf8a6ef8b40f17b6bd1f52f
ilyam8 <ilya@netdata.cloud>
2019-08-23 09:47:52 +0300

    start jobs statuses saver loop

--- orchestrator.go
+++ orchestrator.go
@@ -5,6 +5,7 @@ import (
 	"io"
 	"os"
 	"os/signal"
+	"path"
 	"sync"
 	"syscall"
 	"time"
@@ -18,12 +19,8 @@ import (
 )
 
 var (
-	log = logger.New("plugin", "main", "main")
-
-	defaultConfigPath = multipath.New(
-		os.Getenv("NETDATA_USER_CONFIG_DIR"),
-		os.Getenv("NETDATA_STOCK_CONFIG_DIR"),
-	)
+	log             = logger.New("plugin", "main", "main")
+	jobStatusesFile = "god-jobs-statuses.json"
 )
 
 // Job is an interface that represents a job.
@@ -31,17 +28,11 @@ type Job interface {
 	FullName() string
 	ModuleName() string
 	Name() string
-
-	AutoDetectionRetry() int
-
+	AutoDetection() bool
+	AutoDetectionEvery() int
+	RetryAutoDetection() bool
 	Panicked() bool
-
-	Init() bool
-	Check() bool
-	PostCheck() bool
-
 	Tick(clock int)
-
 	Start()
 	Stop()
 }
@@ -66,14 +57,19 @@ func (c Config) isModuleEnabled(module string, explicit bool) bool {
 // New creates Orchestrator.
 func New() *Orchestrator {
 	return &Orchestrator{
-		ConfigPath:       multipath.New(defaultConfigPath...),
-		Config:           &Config{Enabled: true, DefaultRun: true},
-		Registry:         module.DefaultRegistry,
-		Out:              os.Stdout,
-		modules:          make(module.Registry),
-		jobStartCh:       make(chan Job),
-		jobStartLoopStop: make(chan struct{}),
-		mainLoopStop:     make(chan struct{}),
+		ConfigPath: multipath.New(
+			os.Getenv("NETDATA_USER_CONFIG_DIR"),
+			os.Getenv("NETDATA_STOCK_CONFIG_DIR"),
+		),
+		Config:       &Config{Enabled: true, DefaultRun: true},
+		Registry:     module.DefaultRegistry,
+		Out:          os.Stdout,
+		varLibDir:    os.Getenv("NETDATA_LIB_DIR"),
+		modules:      make(module.Registry),
+		jobStartCh:   make(chan Job),
+		jobStartStop: make(chan struct{}),
+		mainLoopStop: make(chan struct{}),
+		jobsStatuses: &jobsStatuses{mux: new(sync.Mutex), items: make(map[string]map[string]string)},
 	}
 }
 
@@ -87,28 +83,40 @@ type Orchestrator struct {
 	Config               *Config
 	ModulesConfigDirName string
 
+	varLibDir  string
 	configName string
 
-	jobStartCh       chan Job
-	jobStartLoopStop chan struct{}
-	mainLoopStop     chan struct{}
+	jobStartCh   chan Job
+	jobStartStop chan struct{}
+	mainLoopStop chan struct{}
 
-	modules   module.Registry
-	loopQueue loopQueue
+	jobStatusWriter io.Writer
+	jobsStatuses    *jobsStatuses
+	modules         module.Registry
+	loopQueue       loopQueue
 }
 
 // Serve Serve
 func (o *Orchestrator) Serve() {
-	go shutdownTask()
+	go signalHandling()
 
-	if !isatty.IsTerminal(os.Stdout.Fd()) {
-		go heartbeatTask()
+	isAtty := isatty.IsTerminal(os.Stdout.Fd())
+
+	if !isAtty {
+		go keepAlive()
 	}
 
 	go o.jobStartLoop()
 
 	for _, job := range o.createJobs() {
 		o.jobStartCh <- job
+	}
+
+	if !isAtty && o.varLibDir != "" {
+		w := &fileWriter{path: path.Join(o.varLibDir, jobStatusesFile)}
+		s := newJobsStatusesSaver(w, o.jobsStatuses, time.Second*5)
+		go s.mainLoop()
+		defer s.stop()
 	}
 
 	o.mainLoop()
@@ -135,11 +143,11 @@ func (o *Orchestrator) runOnce(clock int) {
 }
 
 func (o *Orchestrator) stop() {
-	o.jobStartLoopStop <- struct{}{}
+	o.jobStartStop <- struct{}{}
 	o.mainLoopStop <- struct{}{}
 }
 
-func shutdownTask() {
+func signalHandling() {
 	signalChan := make(chan os.Signal, 1)
 	signal.Notify(signalChan, syscall.SIGINT, syscall.SIGHUP, syscall.SIGPIPE)
 
@@ -155,7 +163,7 @@ func shutdownTask() {
 	os.Exit(0)
 }
 
-func heartbeatTask() {
+func keepAlive() {
 	t := time.Tick(time.Second)
 	for range t {
 		_, _ = fmt.Fprint(os.Stdout, "\n")
@@ -172,19 +180,6 @@ func (q *loopQueue) add(job Job) {
 	defer q.mux.Unlock()
 
 	q.queue = append(q.queue, job)
-}
-
-func (q *loopQueue) remove(fullName string) Job {
-	q.mux.Lock()
-	defer q.mux.Unlock()
-
-	for i, job := range q.queue {
-		if job.FullName() == fullName {
-			q.queue = append(q.queue[:i], q.queue[i+1:]...)
-			return job
-		}
-	}
-	return nil
 }
 
 func (q *loopQueue) notify(clock int) {
