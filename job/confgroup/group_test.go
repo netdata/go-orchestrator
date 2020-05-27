5742db967872baa72af6c044fcb44d563c4a6135
ilyam8 <ilya@netdata.cloud>
2020-05-27 12:48:39 +0300

    wip

--- job/confgroup/group_test.go
+++ job/confgroup/group_test.go
@@ -1,48 +1,215 @@
 package confgroup
 
-import "testing"
+import (
+	"testing"
 
-// TODO: tech dept
+	"github.com/stretchr/testify/assert"
+)
+
 func TestConfig_Name(t *testing.T) {
+	tests := map[string]struct {
+		cfg      Config
+		expected interface{}
+	}{
+		"string": {
+			cfg:      Config{"name": "name"},
+			expected: "name",
+		},
+		"empty string": {
+			cfg:      Config{"name": ""},
+			expected: "",
+		},
+		"not string": {
+			cfg:      Config{"name": 0},
+			expected: "",
+		},
+		"not set": {
+			cfg:      Config{},
+			expected: "",
+		},
+		"nil cfg": {
+			expected: "",
+		},
+	}
 
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			assert.Equal(t, test.expected, test.cfg.Name())
+		})
+	}
 }
 
-// TODO: tech dept
 func TestConfig_Module(t *testing.T) {
+	tests := map[string]struct {
+		cfg      Config
+		expected interface{}
+	}{
+		"string": {
+			cfg:      Config{"module": "module"},
+			expected: "module",
+		},
+		"empty string": {
+			cfg:      Config{"module": ""},
+			expected: "",
+		},
+		"not string": {
+			cfg:      Config{"module": 0},
+			expected: "",
+		},
+		"not set": {
+			cfg:      Config{},
+			expected: "",
+		},
+		"nil cfg": {
+			expected: "",
+		},
+	}
 
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			assert.Equal(t, test.expected, test.cfg.Module())
+		})
+	}
 }
 
-// TODO: tech dept
 func TestConfig_FullName(t *testing.T) {
+	tests := map[string]struct {
+		cfg      Config
+		expected interface{}
+	}{
+		"name == module": {
+			cfg:      Config{"name": "name", "module": "name"},
+			expected: "name",
+		},
+		"name != module": {
+			cfg:      Config{"name": "name", "module": "module"},
+			expected: "module_name",
+		},
+		"nil cfg": {
+			expected: "",
+		},
+	}
 
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			assert.Equal(t, test.expected, test.cfg.FullName())
+		})
+	}
 }
 
-// TODO: tech dept
 func TestConfig_UpdateEvery(t *testing.T) {
+	tests := map[string]struct {
+		cfg      Config
+		expected interface{}
+	}{
+		"int": {
+			cfg:      Config{"update_every": 1},
+			expected: 1,
+		},
+		"not int": {
+			cfg:      Config{"update_every": "1"},
+			expected: 0,
+		},
+		"not set": {
+			cfg:      Config{},
+			expected: 0,
+		},
+		"nil cfg": {
+			expected: 0,
+		},
+	}
 
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			assert.Equal(t, test.expected, test.cfg.UpdateEvery())
+		})
+	}
 }
 
-// TODO: tech dept
 func TestConfig_AutoDetectionRetry(t *testing.T) {
+	tests := map[string]struct {
+		cfg      Config
+		expected interface{}
+	}{
+		"int": {
+			cfg:      Config{"autodetection_retry": 1},
+			expected: 1,
+		},
+		"not int": {
+			cfg:      Config{"autodetection_retry": "1"},
+			expected: 0,
+		},
+		"not set": {
+			cfg:      Config{},
+			expected: 0,
+		},
+		"nil cfg": {
+			expected: 0,
+		},
+	}
 
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			assert.Equal(t, test.expected, test.cfg.AutoDetectionRetry())
+		})
+	}
 }
 
-// TODO: tech dept
 func TestConfig_Priority(t *testing.T) {
+	tests := map[string]struct {
+		cfg      Config
+		expected interface{}
+	}{
+		"int": {
+			cfg:      Config{"priority": 1},
+			expected: 1,
+		},
+		"not int": {
+			cfg:      Config{"priority": "1"},
+			expected: 0,
+		},
+		"not set": {
+			cfg:      Config{},
+			expected: 0,
+		},
+		"nil cfg": {
+			expected: 0,
+		},
+	}
 
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			assert.Equal(t, test.expected, test.cfg.Priority())
+		})
+	}
 }
 
-// TODO: tech dept
 func TestConfig_Hash(t *testing.T) {
-
+	cfg := Config{"name": "name", "module": "module"}
+	assert.NotZero(t, cfg.Hash())
 }
 
-// TODO: tech dept
 func TestConfig_Set(t *testing.T) {
+	expected := Config{"name": "name"}
 
+	actual := Config{}
+	actual.Set("name", "name")
+
+	assert.Equal(t, expected, actual)
 }
 
-// TODO: tech dept
 func TestConfig_Apply(t *testing.T) {
+	tests := map[string]struct {
+		def         Default
+		origCfg     Config
+		expectedCfg Config
+	}{}
 
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			test.origCfg.Apply(test.def)
+
+			assert.Equal(t, test.expectedCfg, test.origCfg)
+		})
+	}
 }
