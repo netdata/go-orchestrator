762a5aff31168673fa3326dbf4df3bf07b2b6667
ilyam8 <ilya@netdata.cloud>
2020-05-28 18:05:02 +0300

    stop retry tasks on exit

--- job/build/build.go
+++ job/build/build.go
@@ -88,12 +88,7 @@ func NewManager() *Manager {
 
 func (m *Manager) Run(ctx context.Context, in chan []*confgroup.Group) {
 	m.Info("instance is started")
-	defer func() {
-		for _, stop := range *m.retryCache {
-			stop()
-		}
-		m.Info("instance is stopped")
-	}()
+	defer func() { m.cleanup(); m.Info("instance is stopped") }()
 
 	var wg sync.WaitGroup
 
@@ -105,6 +100,12 @@ func (m *Manager) Run(ctx context.Context, in chan []*confgroup.Group) {
 
 	wg.Wait()
 	<-ctx.Done()
+}
+
+func (m *Manager) cleanup() {
+	for _, stop := range *m.retryCache {
+		stop()
+	}
 }
 
 func (m *Manager) runProcessing(ctx context.Context, in <-chan []*confgroup.Group) {
